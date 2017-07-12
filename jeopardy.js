1f8e44078413215e43616b8efbde79f683709e1e
mwlai288 <mwlai288@gmail.com>
2017-07-12 18:26:56 -0400

    added some style and made vertical rows

--- jeopardy.js
+++ jeopardy.js
@@ -31,5 +31,35 @@
     $('#modal1').modal('close');
   });
 
-        
+   
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal2').modal();
+    $('#modal2').modal('open');
+    $('#modal2').modal('close');
+  });
+
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal3').modal();
+    $('#modal3').modal('open');
+    $('#modal3').modal('close');
+  });
+
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal4').modal();
+    $('#modal4').modal('open');
+    $('#modal4').modal('close');
+  });
+
+
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal5').modal();
+    $('#modal5').modal('open');
+    $('#modal5').modal('close');
+  });
+
+
          
