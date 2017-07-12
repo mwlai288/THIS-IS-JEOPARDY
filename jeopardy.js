95dff2add37e28500c73d546574c7c9a1a4a6945
mwlai288 <mwlai288@gmail.com>
2017-07-12 17:17:11 -0400

    added modal to question 1 in HTML

---
+++ jeopardy.js
@@ -0,0 +1,35 @@
+// var questionPrompt = "What is the capital of";
+
+
+// var firstQuestion = {
+// 	wrongQuestion: [Germany, United States, England];
+// 	correctQuestion: [Wales];
+// },
+
+// var secondQuestion = {
+// 	wrongQuestion: [Sweden, Austria, Luxembourg];
+// 	correctQuestion: [Switzerland];	
+// },
+
+// var thirdQuestion = {
+// 	wrongQuestion: [China, Vietnam, Thailand];
+// 	correctQuestion: [Taiwan];
+// },
+
+// var fourthQuestion = {
+// 	wrongQuestion: [Morocco, Ethiopia, Somalia];
+// 	correctQuestion: [Djibouti];
+// };
+
+
+
+
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal1').modal();
+    $('#modal1').modal('open');
+    $('#modal1').modal('close');
+  });
+
+        
+         
