bacba444cde704afa459aa95550e3cd8581767be
mwlai288 <mwlai288@gmail.com>
2017-07-13 16:46:28 -0400

    added alert message after answering

--- jeopardy.js
+++ jeopardy.js
@@ -1,92 +1,141 @@
-// var questionPrompt = "What is the capital of";
-
-// GAME DATA //
 
 
-var questionThings= [
-  {
-    incorrectAnswer1: ["England", "Ireland", "Northern Ireland"];
-    correctAnswer1: ["Wales"];
-    points1: 100;
-  },
 
-  {
-    incorrectAnswer2:
-    correctAnswer2:
-    points2:
-  },
+$(function(){
 
-  {
-    incorrectAnswer3:
-    correctAnswer3:
-    points3:
-  },
+   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal1').modal();
+    $('#modal1').modal('open');
+    $('#modal1').modal('close'); 
+    $('.key').on('click',function(){
+      alert('You are correct!');
+   });
+       
+ $('.modal1').modal();
+    $('#modal1').modal('open');
+    $('#modal1').modal('close'); 
+    $('.keya').on('click',function(){
+      alert('Sorry, you are wrong.');
+   });
 
-  {
-    incorrectAnswer4:
-    correctAnswer4:
-    points4:
-  },
-  {
-    incorrectAnswer5:
-    correctAnswer5:
-    points5:
-  }
+    
+});
 
-];
 
-// var secondQuestion = {
-//  wrongQuestion: [Sweden, Austria, Luxembourg];
-//  correctQuestion: [Switzerland]; 
-// },
 
-// var thirdQuestion = {
-//  wrongQuestion: [China, Vietnam, Thailand];
-//  correctQuestion: [Taiwan];
-// },
+$(function(){
 
-// var fourthQuestion = {
-//  wrongQuestion: [Morocco, Ethiopia, Somalia];
-//  correctQuestion: [Djibouti];
-// };
-
-// GAME //
-
-  // $(document).ready(function(){
-  //   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-  //   $('.modal1').modal();
-  //   $('#modal1').modal('open');
-  //   $('#modal1').modal('close');
-  // });
-
-   
-  $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal2').modal();
     $('#modal2').modal('open');
-    $('#modal2').modal('close');
-  });
+    $('#modal2').modal('close'); 
+    $('.keyb').on('click',function(){
+      alert('You are correct!');
+   });
+       
+ $('.modal2').modal();
+    $('#modal2').modal('open');
+    $('#modal2').modal('close'); 
+    $('.key2').on('click',function(){
+      alert('Sorry, you are wrong.');
+   });
 
-  $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    
+});
+
+
+$(function(){
+
+   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal3').modal();
     $('#modal3').modal('open');
-    $('#modal3').modal('close');
-  });
+    $('#modal3').modal('close'); 
+    $('.keyc').on('click',function(){
+      alert('You are correct!');
+   });
+       
+ $('.modal3').modal();
+    $('#modal3').modal('open');
+    $('#modal3').modal('close'); 
+    $('.key3').on('click',function(){
+      alert('Sorry, you are wrong.');
+   });
 
-  $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    
+});
+
+
+$(function(){
+
+   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal4').modal();
     $('#modal4').modal('open');
-    $('#modal4').modal('close');
-  });
+    $('#modal4').modal('close'); 
+    $('.keyd').on('click',function(){
+      alert('You are correct!');
+   });
+       
+ $('.modal4').modal();
+    $('#modal4').modal('open');
+    $('#modal4').modal('close'); 
+    $('.key4').on('click',function(){
+      alert('Sorry, you are wrong.');
+   });
 
+    
+});
 
-  $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+$(function(){
+
+   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal5').modal();
     $('#modal5').modal('open');
-    $('#modal5').modal('close');
-  });
+    $('#modal5').modal('close'); 
+    $('.keye').on('click',function(){
+      alert('You are correct!');
+   });
+       
+ $('.modal5').modal();
+    $('#modal5').modal('open');
+    $('#modal5').modal('close'); 
+    $('.key5').on('click',function(){
+      alert('Sorry, you are wrong.');
+   });
+
+    
+});
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ]
