95a675aeab8af7494676a3b40ee61076f765c328
mwlai288 <mwlai288@gmail.com>
2017-07-13 11:39:28 -0400

    fixed objects data

--- jeopardy.js
+++ jeopardy.js
@@ -1,35 +1,63 @@
 // var questionPrompt = "What is the capital of";
 
+// GAME DATA //
 
-// var firstQuestion = {
-// 	wrongQuestion: [Germany, United States, England];
-// 	correctQuestion: [Wales];
-// },
+
+var questionThings= [
+  {
+    incorrectAnswer1: ["England", "Ireland", "Northern Ireland"];
+    correctAnswer1: ["Wales"];
+    points1: 100;
+  },
+
+  {
+    incorrectAnswer2:
+    correctAnswer2:
+    points2:
+  },
+
+  {
+    incorrectAnswer3:
+    correctAnswer3:
+    points3:
+  },
+
+  {
+    incorrectAnswer4:
+    correctAnswer4:
+    points4:
+  },
+  {
+    incorrectAnswer5:
+    correctAnswer5:
+    points5:
+  }
+
+];
 
 // var secondQuestion = {
-// 	wrongQuestion: [Sweden, Austria, Luxembourg];
-// 	correctQuestion: [Switzerland];	
+//  wrongQuestion: [Sweden, Austria, Luxembourg];
+//  correctQuestion: [Switzerland]; 
 // },
 
 // var thirdQuestion = {
-// 	wrongQuestion: [China, Vietnam, Thailand];
-// 	correctQuestion: [Taiwan];
+//  wrongQuestion: [China, Vietnam, Thailand];
+//  correctQuestion: [Taiwan];
 // },
 
 // var fourthQuestion = {
-// 	wrongQuestion: [Morocco, Ethiopia, Somalia];
-// 	correctQuestion: [Djibouti];
+//  wrongQuestion: [Morocco, Ethiopia, Somalia];
+//  correctQuestion: [Djibouti];
 // };
 
+// GAME //
 
-
-
-  $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal1').modal();
-    $('#modal1').modal('open');
-    $('#modal1').modal('close');
-  });
+  // $(document).ready(function(){
+  //   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  //   $('.modal1').modal();
+  //   $('#modal1').modal('open');
+  //   $('#modal1').modal('close');
+  // });
 
    
   $(document).ready(function(){
@@ -62,4 +90,3 @@
   });
 
 
-         
